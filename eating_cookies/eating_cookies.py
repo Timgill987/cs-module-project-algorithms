344dc3be2a2b10b1456cc065850d36d4b67cd304
Timothy Gilliam <timgill987@gmail.com>
2020-07-23 09:35:12 -0600

    got cookies to work for the small test but not the large one

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,6 +4,14 @@ Returns: an integer
 '''
 def eating_cookies(n):
 
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
